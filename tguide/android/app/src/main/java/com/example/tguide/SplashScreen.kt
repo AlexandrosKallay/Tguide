895ff22b854af3e646a6d5a4188832a3593c7b61
Alexandros <a.kallay97@gmail.com>
2021-11-17 19:15:05 +0200

    splashscreen completed

--- tguide/android/app/src/main/java/com/example/tguide/SplashScreen.kt
+++ tguide/android/app/src/main/java/com/example/tguide/SplashScreen.kt
@@ -1,54 +1,43 @@
 package com.example.tguide
-
-import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tguide.MainActivity
 
 class SplashScreen : AppCompatActivity() {
+    var progressBar: ProgressBar? = null
+    //var textView: TextView? = null
+    var value = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        progressBar = findViewById(R.id.progressBarId);
-        textView = findViewById(R.id.textViewId);
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (value = 0; value<=100; value++){
+        progressBar = findViewById(R.id.progressBarId)
+        //textView = findViewById(R.id.text1)
+        val thread = Thread {
+            value = 0
+            while (value <= 100) {
                 try {
-                    Thread.sleep(30);
-                    progressBar.setProgress(value);
-                    setText(textView, String.valueOf(value)+"%" );
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    Thread.sleep(30)
+                    progressBar?.setProgress(value)
+                    //setText(textView, "$value%")
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
                 }
+                value++
             }
-            }
-        });
-
-        thread.start();
-
-        int secondsDelayed = 3;
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                startActivity(new Intent(SplashScreen.this,   MainActivity.class));
-                finish();
-            }
-        }, secondsDelayed * 1000);
-
+        }
+        thread.start()
+        val secondsDelayed = 3
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }, (secondsDelayed * 1000).toLong())
     }
 
-    private void setText(final TextView text,final String value){
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                text.setText(value);
-            }
-        });
+    private fun setText(text: TextView?, value: String) {
+        runOnUiThread { text!!.text = value }
     }
-
 }
