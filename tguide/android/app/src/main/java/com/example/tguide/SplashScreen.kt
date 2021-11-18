6c7ebe6f123e61aa72fbaaf928296d2e8802a9d9
Alexandros <a.kallay97@gmail.com>
2021-11-18 16:59:57 +0200

    splash screen bugs

--- tguide/android/app/src/main/java/com/example/tguide/SplashScreen.kt
+++ tguide/android/app/src/main/java/com/example/tguide/SplashScreen.kt
@@ -1,7 +1,12 @@
 package com.example.tguide
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -9,33 +14,58 @@ import com.example.tguide.MainActivity
 
 class SplashScreen : AppCompatActivity() {
     var progressBar: ProgressBar? = null
-    //var textView: TextView? = null
+    var image: ImageView? = null
+    var textView: TextView? = null
     var value = 0
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         progressBar = findViewById(R.id.progressBarId)
-        //textView = findViewById(R.id.text1)
+        image = findViewById(R.id.imglogo)
+        textView = findViewById(R.id.text1)
+        ObjectAnimator.ofFloat(image, View.ALPHA, 0.2f, 1.0f).setDuration(2000).start();
+        ObjectAnimator.ofFloat(textView, View.ALPHA, 0.2f, 1.0f).setDuration(2000).start();
+        ObjectAnimator.ofFloat(progressBar, View.ALPHA, 0.2f, 1.0f).setDuration(2000).start();
+
+
         val thread = Thread {
             value = 0
             while (value <= 100) {
+
                 try {
-                    Thread.sleep(30)
+
+                    Thread.sleep(25)
                     progressBar?.setProgress(value)
+
+
+
+
                     //setText(textView, "$value%")
                 } catch (e: InterruptedException) {
                     e.printStackTrace()
                 }
                 value++
             }
+
         }
         thread.start()
         val secondsDelayed = 3
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@SplashScreen, MainActivity::class.java))
             finish()
+
         }, (secondsDelayed * 1000).toLong())
+
+
+
     }
+
+
+
+
+
+
 
     private fun setText(text: TextView?, value: String) {
         runOnUiThread { text!!.text = value }
